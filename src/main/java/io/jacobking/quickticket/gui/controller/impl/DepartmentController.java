5871c3ec2e753cb2525207f4edc44f7c5f107357
jkingster <jkingster@protonmail.com>
2024-04-04 15:49:59 -0500

    Finalize department controller.

--- src/main/java/io/jacobking/quickticket/gui/controller/impl/DepartmentController.java
+++ src/main/java/io/jacobking/quickticket/gui/controller/impl/DepartmentController.java
@@ -6,6 +6,8 @@ import io.jacobking.quickticket.gui.controller.Controller;
 import io.jacobking.quickticket.gui.model.impl.CompanyModel;
 import io.jacobking.quickticket.gui.model.impl.DepartmentModel;
 import io.jacobking.quickticket.tables.pojos.Department;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.util.StringConverter;
@@ -16,70 +18,79 @@ import java.util.ResourceBundle;
 
 public class DepartmentController extends Controller {
 
-    @FXML private SearchableComboBox<CompanyModel>    companyComboBox;
-    @FXML private SearchableComboBox<DepartmentModel> departmentComboBox;
+    @FXML private CheckBox newDepartmentCheckBox;
 
-    @FXML private CheckBox noCompanyCheckBox;
+    @FXML private SearchableComboBox<CompanyModel>    companySearchBox;
+    @FXML private SearchableComboBox<DepartmentModel> departmentSearchBox;
 
     @FXML private TextField nameField;
     @FXML private TextField emailField;
-
-    @FXML private TextArea descriptionArea;
+    @FXML private TextArea  descriptionArea;
 
     @FXML private Button createButton;
     @FXML private Button updateButton;
     @FXML private Button deleteButton;
 
     @Override public void initialize(URL url, ResourceBundle resourceBundle) {
-        configureCompanyComboBox();
-        configureDepartmentComboBox();
-        configureNoCompanyCheckBox();
+        configureNewDepartmentCheckBox();
+        configureCompanySearchBox();
+        configureDepartmentSearchBox();
         configureButtons();
     }
 
     @FXML private void onCreate() {
-        final String departmentName = nameField.getText();
-        if (departmentName.isEmpty()) {
-            Alerts.showError("Failed!", "Could not create department.", "A value in the name field is required!");
+        final String name = nameField.getText();
+        if (name.isEmpty()) {
+            Alerts.showError(
+                    "Failed!",
+                    "Could not create department.",
+                    "Department name is required!"
+            );
             return;
         }
 
-        final Department newDepartment = new Department()
-                .setName(departmentName)
-                .setEmail(emailField.getText())
-                .setDescription(descriptionArea.getText())
-                .setCompanyId(getCompanyId());
-
-        final DepartmentModel departmentModel = department.createModel(newDepartment);
-        if (departmentModel != null) {
-            Notifications.showInfo("Success!", "Department created successfully!");
-            clearFields();
+        final DepartmentModel newDepartment = department.createModel(getNewDepartment());
+        if (newDepartment != null) {
+            Notifications.showInfo("Success!", "Department has been created/");
         }
     }
 
     @FXML private void onUpdate() {
-        final DepartmentModel departmentModel = departmentComboBox.getSelectionModel().getSelectedItem();
-        if (departmentModel == null)
+        final DepartmentModel departmentModel = departmentSearchBox.getSelectionModel().getSelectedItem();
+        if (departmentModel == null) {
+            Alerts.showError(
+                    "Failure!",
+                    "Could not update department.",
+                    "No department selected!"
+            );
             return;
+        }
 
-        departmentModel.setCompanyId(getCompanyId());
-        departmentModel.setDescription(descriptionArea.getText());
         departmentModel.setName(nameField.getText());
         departmentModel.setEmail(emailField.getText());
+        departmentModel.setDescription(descriptionArea.getText());
+        departmentModel.setCompanyId(getCompanyId());
 
         if (department.update(departmentModel)) {
-            Notifications.showInfo("Update Successful!", "This department model was updated.");
+            Notifications.showInfo("Success!", "Department updated successfully.");
         }
     }
 
     @FXML private void onDelete() {
-        final DepartmentModel departmentModel = departmentComboBox.getSelectionModel().getSelectedItem();
-        if (departmentModel == null)
+        final DepartmentModel departmentModel = departmentSearchBox.getSelectionModel().getSelectedItem();
+        if (departmentModel == null) {
+            Alerts.showError(
+                    "Failure!",
+                    "Could not delete department.",
+                    "No department selected!"
+            );
             return;
+        }
 
         Alerts.showConfirmation(() -> deleteDepartment(departmentModel),
-                "Are you sure you want to delete this department?",
-                "This action cannot be undone!").ifPresent(type -> {
+                "Are you sure you want to do this?",
+                "This department cannot be recovered if deleted."
+        ).ifPresent(type -> {
             if (type == ButtonType.YES) {
                 deleteDepartment(departmentModel);
             }
@@ -88,63 +99,68 @@ public class DepartmentController extends Controller {
 
     private void deleteDepartment(final DepartmentModel departmentModel) {
         department.remove(departmentModel.getId());
+        Notifications.showWarning("Success", "Department has been deleted.");
+    }
+
+    @FXML private void onReset() {
         clearFields();
     }
 
-    private void configureCompanyComboBox() {
-        companyComboBox.setCellFactory(data -> new ListCell<>() {
-            @Override protected void updateItem(CompanyModel companyModel, boolean b) {
-                super.updateItem(companyModel, b);
-                if (b || companyModel == null) {
-                    setText(null);
-                    return;
-                }
+    private void configureNewDepartmentCheckBox() {
+        departmentSearchBox.disableProperty().bind(newDepartmentCheckBox.selectedProperty());
 
-                setText(companyModel.getName());
+        newDepartmentCheckBox.selectedProperty().addListener(((observableValue, aBoolean, t1) -> {
+            if (t1 != null && t1) {
+                departmentSearchBox.getSelectionModel().clearSelection();
+                nameField.clear();
+                descriptionArea.clear();
+                emailField.clear();
             }
-        });
+        }));
+    }
 
-        companyComboBox.setConverter(new StringConverter<>() {
+    private void configureCompanySearchBox() {
+        companySearchBox.setConverter(new StringConverter<>() {
             @Override public String toString(CompanyModel companyModel) {
-                return (companyModel == null) ? "" : companyModel.getName();
+                return companyModel != null ? companyModel.getName() : "";
             }
 
             @Override public CompanyModel fromString(String s) {
                 return null;
             }
         });
-
-        companyComboBox.setItems(company.getObservableList());
-
-        companyComboBox.getSelectionModel().selectedItemProperty()
-                .addListener(((observableValue, companyModel, t1) -> {
-                    if (t1 == null || t1.getId() == 0) {
-                        departmentComboBox.setItems(department.getObservableList());
-                        return;
-                    }
-
-                    final int companyId = t1.getId();
-                    departmentComboBox.setItems(department.getObservableListByFilter((
-                            departmentModel -> departmentModel.getCompanyId() == companyId
-                    )));
-                }));
-    }
-
-    private void configureDepartmentComboBox() {
-        departmentComboBox.setCellFactory(data -> new ListCell<>() {
-            @Override protected void updateItem(DepartmentModel departmentModel, boolean b) {
-                super.updateItem(departmentModel, b);
-                if (b || departmentModel == null) {
-                    setGraphic(null);
+        companySearchBox.setCellFactory(data -> new ListCell<>() {
+            @Override protected void updateItem(CompanyModel companyModel, boolean b) {
+                super.updateItem(companyModel, b);
+                if (b || companyModel == null) {
+                    setText(null);
                     return;
                 }
-                setText(String.format("ID: %s | %s", departmentModel.getId(), departmentModel.getName()));
+                setText(String.format("ID: %s | %s", companyModel.getId(), companyModel.getName()));
             }
         });
+        companySearchBox.getSelectionModel().selectedItemProperty().addListener(((observableValue, companyModel, t1) -> {
+            if (t1 == null)
+                return;
 
-        departmentComboBox.setConverter(new StringConverter<>() {
+            final int companyId = t1.getId();
+            if (companyId == 0) {
+                departmentSearchBox.setItems(department.getObservableList()
+                        .filtered(dm -> dm.getId() != 0)
+                );
+            } else if (companyId > 0) {
+                departmentSearchBox.setItems(department.getObservableListByFilter(
+                        dm -> dm.getCompanyId() == companyId
+                ));
+            }
+        }));
+        companySearchBox.setItems(company.getObservableList());
+    }
+
+    private void configureDepartmentSearchBox() {
+        departmentSearchBox.setConverter(new StringConverter<>() {
             @Override public String toString(DepartmentModel departmentModel) {
-                return (departmentModel == null) ? "" : departmentModel.getName();
+                return departmentModel != null ? departmentModel.getName() : "";
             }
 
             @Override public DepartmentModel fromString(String s) {
@@ -152,40 +168,41 @@ public class DepartmentController extends Controller {
             }
         });
 
-        departmentComboBox.setItems(department.getObservableListByFilter(dm -> dm.getId() != 0));
-        departmentComboBox.getSelectionModel().selectedItemProperty()
-                .addListener(((observableValue, departmentModel, t1) -> {
-                    if (t1 == null) {
-                        clearFields();
-                        return;
-                    }
-                    populateFields(t1);
-                }));
-    }
-
-    private void configureButtons() {
-        createButton.disableProperty().bind(departmentComboBox.getSelectionModel().selectedItemProperty().isNotNull());
-
-        updateButton.disableProperty().bind(createButton.disabledProperty().not());
-        deleteButton.disableProperty().bind(createButton.disabledProperty().not());
-    }
-
-    private void configureNoCompanyCheckBox() {
-        noCompanyCheckBox.selectedProperty().addListener(((observableValue, aBoolean, t1) -> {
-            companyComboBox.disableProperty().bind(observableValue);
-
-            if (t1) {
-                departmentComboBox.setItems(department.getObservableListByFilter(dm -> dm.getId() != 0));
+        departmentSearchBox.setCellFactory(data -> new ListCell<>() {
+            @Override protected void updateItem(DepartmentModel departmentModel, boolean b) {
+                super.updateItem(departmentModel, b);
+                if (b || departmentModel == null) {
+                    setText(null);
+                    return;
+                }
+                setText(String.format("ID: %s | %s", departmentModel.getId(), departmentModel.getName()));
             }
+        });
+
+        departmentSearchBox.getSelectionModel().selectedItemProperty().addListener(((observableValue, departmentModel, t1) -> {
+            if (t1 == null) {
+                return;
+            }
+            if (newDepartmentCheckBox.isSelected())
+                return;
+
+            populateFields(t1);
         }));
     }
 
-    private void clearFields() {
-        nameField.clear();
-        emailField.clear();
-        descriptionArea.clear();
-        departmentComboBox.getSelectionModel().clearSelection();
-        companyComboBox.getSelectionModel().clearSelection();
+    private void configureButtons() {
+        final BooleanBinding firstConditionCheck = Bindings.and(newDepartmentCheckBox.selectedProperty(),
+                companySearchBox.getSelectionModel().selectedItemProperty().isNotNull()).not();
+
+        final BooleanBinding secondConditionCheck = Bindings.or(
+                companySearchBox.getSelectionModel().selectedItemProperty().isNotNull(),
+                departmentSearchBox.getSelectionModel().selectedItemProperty().isNotNull()
+        ).not();
+
+        createButton.disableProperty().bind(firstConditionCheck.or(secondConditionCheck));
+
+        deleteButton.disableProperty().bind(createButton.disabledProperty().not());
+        updateButton.disableProperty().bind(createButton.disabledProperty().not());
     }
 
     private void populateFields(final DepartmentModel departmentModel) {
@@ -194,12 +211,25 @@ public class DepartmentController extends Controller {
         descriptionArea.setText(departmentModel.getDescription());
     }
 
-    private int getCompanyId() {
-        final CompanyModel companyModel = companyComboBox.getSelectionModel().getSelectedItem();
-        return companyModel == null ? 0 : companyModel.getId();
+    private void clearFields() {
+        nameField.clear();
+        emailField.clear();
+        descriptionArea.clear();
+        departmentSearchBox.getSelectionModel().clearSelection();
+        companySearchBox.getSelectionModel().clearSelection();
     }
 
-    @FXML private void onReset() {
-        clearFields();
+    private int getCompanyId() {
+        return companySearchBox.getSelectionModel()
+                .getSelectedItem()
+                .getId();
+    }
+
+    private Department getNewDepartment() {
+        return new Department()
+                .setName(nameField.getText())
+                .setEmail(emailField.getText())
+                .setDescription(descriptionArea.getText())
+                .setCompanyId(getCompanyId());
     }
 }
