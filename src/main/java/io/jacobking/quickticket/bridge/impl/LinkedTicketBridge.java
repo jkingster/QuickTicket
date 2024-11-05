440dcfc2cc212c0bda75ac9c3d0d7269a4e8c3e9
jkingster <jkingster@protonmail.com>
2024-11-05 15:19:55 -0600

    Get rid of no longer wanted tables, remove old settings.

--- src/main/java/io/jacobking/quickticket/bridge/impl/LinkedTicketBridge.java
+++ src/main/java/io/jacobking/quickticket/bridge/impl/LinkedTicketBridge.java
@@ -3,7 +3,6 @@ package io.jacobking.quickticket.bridge.impl;
 import io.jacobking.quickticket.bridge.Bridge;
 import io.jacobking.quickticket.core.database.Database;
 import io.jacobking.quickticket.core.database.repository.RepoType;
-import io.jacobking.quickticket.gui.model.impl.LinkedTicketModel;
 import io.jacobking.quickticket.tables.pojos.LinkedTicket;
 import javafx.collections.ObservableList;
 
